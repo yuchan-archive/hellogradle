7789dd7752d80676aec5a59228d1f5cdc75a782e
yuchan <yusuke@junkpiano.me>
2014-12-18 14:52:57 +0900

    initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yusuke on 12/18/14.
+ */
+
+import com.google.common.base.Strings;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(Strings.repeat("*", 10));
+    }
+}
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by yusuke on 12/18/14.
+ */
+public class HelloTest {
+    @Test
+    public void testHello() {
+        assertTrue(true);
+    }
+}
